41cf98eeda19bdf21a0dd586d69949817a64e698
Frnak <francisco@glide.com>
2020-11-19 19:21:05 -0300

    Updated auto refresh

--- client/src/components/Timezone.js
+++ client/src/components/Timezone.js
@@ -5,16 +5,26 @@ import moment from 'moment';
 export default function Timezone({ timezoneName }) {
   const [timezone, setTimezone] = useState({});
 
+  const updateTime = () =>
+    setTimeout(() => {
+      getTimezone(timezoneName).then((response) => {
+        setTimezone(response.data);
+        updateTime();
+      });
+    }, 5000);
+
   useEffect(() => {
+    updateTime();
     getTimezone(timezoneName).then((response) => {
       setTimezone(response.data);
     });
   }, []);
+
   return (
     <div>
       <p>{timezone.timezone}</p>
       <p>{moment(timezone.datetime).format('MM/DD/YYYY')}</p>
-      <p>{moment(timezone.datetime).format('HH:mm')}</p>
+      <p>{moment(timezone.datetime).format('HH:mm:ss')}</p>
     </div>
   );
 }
