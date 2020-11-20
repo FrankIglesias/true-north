c41e0d77f2de227582ae9b04ed513c336f901c66
Frnak <francisco@glide.com>
2020-11-20 18:11:41 -0300

    Created setInteval hook

--- client/src/components/Timezone.js
+++ client/src/components/Timezone.js
@@ -1,29 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { getTimezone } from '../services/timezone';
+import { useInterval } from '../hooks/useInterval';
 import moment from 'moment';
 import styles from './Timezone.module.scss';
 
 export default function Timezone({ timezoneName, removeCurrent }) {
   const [timezone, setTimezone] = useState(null);
-  const [updater, setUpdater] = useState(null);
 
-  const updateTime = () =>
-    setTimeout(() => {
-      getTimezone(timezoneName).then((response) => {
-        setTimezone(response.data);
-        setUpdater(updateTime());
-      });
-    }, 5000);
+  useInterval(() => {
+    getTimezone(timezoneName).then((response) => {
+      setTimezone(response.data);
+    });
+  }, 5000);
 
   useEffect(() => {
     getTimezone(timezoneName).then((response) => {
       setTimezone(response.data);
-      setUpdater(updateTime());
     });
-    return () => {
-      setUpdater(null);
-      clearTimeout(updater);
-    };
   }, []);
 
   if (!timezone) {
@@ -38,16 +31,12 @@ export default function Timezone({ timezoneName, removeCurrent }) {
       <p className={styles.timezoneValues}>{timezoneName}</p>
       <p className={styles.timezoneValues}>
         {moment(timezone.datetime)
-          
           .utcOffset(timezone.utc_offset)
-          
           .format('MM/DD/YYYY')}
       </p>
       <p className={styles.timezoneValues}>
         {moment(timezone.datetime)
-          
           .utcOffset(timezone.utc_offset)
-          
           .format('HH:mm:ss')}
       </p>
     </div>
